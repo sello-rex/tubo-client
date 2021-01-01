f9f973b99ecca152ee64bc9fbbb60cdb84e3c4af
Sello Ramolelle <selloram@gmail.com>
2021-01-01 13:53:01 +0200

    add google analytics

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tube-client
+[![Netlify Status](https://api.netlify.com/api/v1/badges/b3b1e99d-52f0-4c9c-a10a-43f158aedb1c/deploy-status)](https://app.netlify.com/sites/tubo-client/deploys)
--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import PageInfo from './components/PageInfo';
 import SearchBar from './components/Search';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-186355540-1');
 
 function App() {
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className="container-lg">
       <Navbar />
