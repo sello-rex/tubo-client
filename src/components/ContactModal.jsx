2e2df9dcea5b01e19360bf2d41b98ccecfb371a1
Sello Ramolelle <selloram@gmail.com>
2021-01-02 17:25:07 +0200

    add netlify form handler

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -1,16 +1,62 @@
 import React, { Component } from 'react'
-
+import Swal from 'sweetalert2';
 class ContactModal extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      category: 'suggestion'
+      category: 'suggestion',
+      name: '',
+      email: '',
+      message: ''
     }
   }
 
   handleCategoryChange = event => {
     this.setState( { category: event.target.value } );
+  }
+
+  handleInput = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  encode = obj => {
+    return Object.keys(obj)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
+      .join("&");
+  }
+
+  onSubmit = event =>{
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: this.encode({ "form-name": "contact-us", ...this.state })
+    })
+    .then(() =>{
+      Swal.fire({
+        toast: true,
+        title: "Message sent!!",
+        icon: 'success',
+        timer: 4000,
+        position: 'bottom-end',
+        showConfirmButton: false,
+      })
+    })
+    .catch(error => {
+      Swal.fire({
+        toast: true,
+        position: 'bottom-end',
+        showConfirmButton: false,
+        title: error.message,
+        icon: 'error',
+        timer: 4000
+      })
+    }).finally(()=>{
+      document.querySelector('.btn-close').click()
+      console.log('done!!!')
+    });
+
+    event.preventDefault();
   }
 
   render() {
@@ -27,18 +73,18 @@ class ContactModal extends Component {
             <h4 className="modal-title">Have something to say?</h4>
             <h5 className="modal-subtitle">Speak your mind, we are here to listen.</h5>
             <p className="modal-text">You do no need to tell us your name or your email, we just want to know your thoughts, though they would be great have so we can reply to you.</p>
-            <form name="contact-use-form" data-netlify="true">
+            <form onSubmit={ this.onSubmit } name="contact-us-form" data-netlify="true">
               <div className="form-group">
                 <label htmlFor="name">Name <small>(optional)</small></label>
-                <input className="input-block" type="text" name="name" id="name" />
+                <input className="input-block" onChange={ this.handleInput } value={ this.state.name } type="text" name="name" id="name" />
               </div>
               <div className="form-group">
                 <label htmlFor="email">Email <small>(optional)</small></label>
-                <input className="input-block" type="email" name="email" id="email" />
+                <input className="input-block" onChange={ this.handleInput } value={ this.state.email }  type="email" name="email" id="email" />
               </div>
               <div className="form-group">
                 <label htmlFor="category">Category *</label>
-                <select onChange={ this.handleCategoryChange } className="input-block" defaultValue="suggestion" name="category" id="category">
+                <select required onChange={ this.handleCategoryChange } className="input-block" defaultValue="suggestion" name="category" id="category">
                   <option value="compliment">Compliment</option>
                   <option value="complaint">Complaint</option>
                   <option value="suggestion">Suggestion</option>
@@ -46,10 +92,10 @@ class ContactModal extends Component {
               </div>
               <div className="form-group">
                 <label htmlFor="message">{ this.state.category } *</label>
-                <textarea className="input-block" id="message" placeholder={ `What's your ${this.state.category}` }></textarea>
+                <textarea required className="input-block" name="message" value={ this.state.message } onChange={ this.handleInput } id="message" placeholder={ `What's your ${this.state.category}` }></textarea>
               </div>
               <div className="form-group">
-                <button className="btn-block">Send { this.state.category }</button>
+                <button type="submit" className="btn-block">Send { this.state.category }</button>
               </div>
             </form>
           </div>
